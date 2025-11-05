2250bce0c9b284d199f3dbb810fefdad78ec7a35
antonellaartoni <artoniantonella01@gmail.com>
2025-11-04 23:31:15 -0300

    create oauth_google.sample.php

---
+++ config/oauth_google.sample.php
@@ -0,0 +1,15 @@
+<?php
+// config/oauth_google.sample.php
+
+// URL de redireccionamiento de Google OAuth 2.0
+define('GOOGLE_REDIRECT_URL', 'http://localhost/utnproject/pages/oauth_google_callback.php');
+
+// ID de cliente de Google
+// Reemplaza 'TU_CLIENT_ID' con tu ID de cliente real de Google Cloud.
+define('GOOGLE_CLIENT_ID', 'TU_CLIENT_ID');
+
+// Secreto de cliente de Google
+// Reemplaza 'TU_CLIENT_SECRET' con tu secreto de cliente real de Google Cloud.
+define('GOOGLE_CLIENT_SECRET', 'TU_CLIENT_SECRET');
+
+?>
