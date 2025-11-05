43b88499019c9b89a798c162a95cc2944ff4bc8c
antonellaartoni <artoniantonella01@gmail.com>
2025-11-04 23:50:01 -0300

    update oauth_google_callback.php

--- pages/oauth_google_callback.php
+++ pages/oauth_google_callback.php
@@ -109,7 +109,7 @@ try {
     $_SESSION['logged_in'] = true;
 
     // Redirigir al usuario a la página principal de la aplicación
-    header('Location: home.html');
+    header('Location: /utnproject/pages/home.html');
     exit();
 
 } catch (PDOException $e) {
